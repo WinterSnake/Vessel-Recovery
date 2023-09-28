80280c70029cbe0760cb48c28d1d20afd694576e
Winter_Snake <blizzardsnake98@gmail.com>
2023-09-27 23:50:07 -0400

    add: git and core files

---
+++ Plugin.VesselRecovery/VesselRecovery.cs
@@ -0,0 +1,15 @@
+﻿/*
+    Vessel Recovery
+
+    Written By: Ryan Smith
+*/
+using System;
+using UnityEngine;
+
+namespace VesselRecovery;
+
+[KSPAddon(KSPAddon.Startup.Flight, false)]
+public class VesselRecovery : MonoBehaviour
+{
+    /* Instance Methods */
+}
